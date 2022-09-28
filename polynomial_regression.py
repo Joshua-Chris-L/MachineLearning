869db278551a22dd587569a9fcd06d2d41cdbaa4
Joshua Christopher <joshchrislight@gmail.com>
2022-09-28 14:30:44 -0700

    Update polynomial_regression.py

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
-# coding: utf-8
-
 # # Polynomial Regression
 
 # ## Importing the libraries
-
-# In[7]:
-
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,30 +9,18 @@ import pandas as pd
 
 
 # ## Importing the dataset
-
-# In[8]:
-
-
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 
 # ## Training the Linear Regression model on the whole dataset
-
-# In[9]:
-
-
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 
 
 # ## Training the Polynomial Regression model on the whole dataset
-
-# In[18]:
-
-
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
@@ -46,10 +29,6 @@ lin_reg_2.fit(X_poly, y)
 
 
 # ## Visualising the Linear Regression results
-
-# In[19]:
-
-
 plt.scatter(X, y, color = 'red')
 plt.plot(X, lin_reg.predict(X), color = 'blue')
 plt.title('Truth or Bluff (Linear Regression)')
@@ -59,10 +38,6 @@ plt.show()
 
 
 # ## Visualising the Polynomial Regression results
-
-# In[20]:
-
-
 plt.scatter(X, y, color = 'red')
 plt.plot(X, lin_reg_2.predict(X_poly), color = 'blue')
 plt.title('Truth or Bluff (Polynomial Regression)')
@@ -72,10 +47,6 @@ plt.show()
 
 
 # ## Visualising the Polynomial Regression results (for higher resolution and smoother curve)
-
-# In[21]:
-
-
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
@@ -87,17 +58,9 @@ plt.show()
 
 
 # ## Predicting a new result with Linear Regression
-
-# In[8]:
-
-
 lin_reg.predict([[6.5]])
 
 
 # ## Predicting a new result with Polynomial Regression
-
-# In[9]:
-
-
 lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
 
