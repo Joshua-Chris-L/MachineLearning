a2e69569373e1439ed8cf673e31759bb61dbc0a0
Joshua Christopher <joshchrislight@gmail.com>
2022-10-01 10:33:23 +0200

    Update decision_tree_regression.py

--- decision_tree_regression.py
+++ decision_tree_regression.py
@@ -4,46 +4,28 @@
 # # Decision Tree Regression
 
 # ## Importing the libraries
-
-# In[6]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
 # ## Importing the dataset
-
-# In[7]:
-
-
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 
 # ## Training the Decision Tree Regression model on the whole dataset
-
-# In[8]:
-
-
 from sklearn.tree import DecisionTreeRegressor
 regressor = DecisionTreeRegressor(random_state = 0)
 regressor.fit(X, y)
 
 
 # ## Predicting a new result
-
-# In[11]:
-
-
 regressor.predict([[6.5]])
 
 
 # In[13]:
-
-
 plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
 plt.title('Truth or Bluff (Decision Tree Regression)')
