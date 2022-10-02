8cb9a868e7bbfabfe42b55b9844933113643e9c0
Joshua Christopher <joshchrislight@gmail.com>
2022-10-02 21:04:46 +0200

    Update random_forest_regression.py

--- random_forest_regression.py
+++ random_forest_regression.py
@@ -4,29 +4,18 @@
 # # Random Forest Regression
 
 # ## Importing the libraries
-
-# In[49]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
 # ## Importing the dataset
-
-# In[50]:
-
-
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 
 # ## Training the Random Forest Regression model on the whole dataset
-
-# In[51]:
-
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import make_regression
@@ -44,17 +33,11 @@ regr.predict([[6.5]])
 
 # ## Predicting a new result
 
-# In[54]:
-
 
 regr.predict([[6.5]])
 
 
 # ## Visualising the Random Forest Regression results (higher resolution)
-
-# In[48]:
-
-
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
@@ -64,8 +47,6 @@ plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
-
-# In[ ]:
 
 
 
