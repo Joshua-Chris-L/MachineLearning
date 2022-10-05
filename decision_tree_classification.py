b2186e75e060b745a54ea3187ae9e3c816f3255e
Joshua Christopher <joshchrislight@gmail.com>
2022-10-04 18:18:39 -0700

    Update decision_tree_classification.py

--- decision_tree_classification.py
+++ decision_tree_classification.py
@@ -14,103 +14,51 @@ import pandas as pd
 
 
 # ## Importing the dataset
-
-# In[2]:
-
-
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 
 # ## Splitting the dataset into the Training set and Test set
-
-# In[3]:
-
-
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-
-# In[4]:
-
-
 print(X_train)
-
-
-# In[5]:
-
 
 print(y_train)
 
-
-# In[6]:
-
-
 print(X_test)
-
-
-# In[7]:
-
 
 print(y_test)
 
 
 # ## Feature Scaling
-
-# In[8]:
-
-
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-
-# In[9]:
-
-
 print(X_train)
-
-
-# In[10]:
-
 
 print(X_test)
 
 
 # ## Training the Decision Tree Classification model on the Training set
-
-# In[11]:
-
-
 from sklearn.tree import DecisionTreeClassifier
 classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
 classifier.fit(X_train, y_train)
 
 
 # ## Predicting a new result
-
-# In[12]:
-
-
 print(classifier.predict(sc.transform([[30,87000]])))
 
 
 # ## Predicting the Test set results
-
-# In[13]:
-
-
 y_pred = classifier.predict(X_test)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 
 # ## Making the Confusion Matrix
-
-# In[14]:
-
-
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
@@ -118,10 +66,6 @@ accuracy_score(y_test, y_pred)
 
 
 # ## Visualising the Training set results
-
-# In[15]:
-
-
 from matplotlib.colors import ListedColormap
 X_set, y_set = sc.inverse_transform(X_train), y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
